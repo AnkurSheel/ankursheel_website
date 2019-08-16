ddf786e1ae468003b403fcef6634398595d45380
Ankur Sheel <ankur.sheel@gmail.com>
2019-08-16 20:10:30 +1200

    change TagList to tsx and remove usage of styled. change styling

---
+++ src/components/TagList.tsx
@@ -0,0 +1,52 @@
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+import React from 'react';
+import { colors } from '../tokens';
+
+const styles = {
+    tagsList: css({
+        color: `${colors.textLight}`,
+        display: 'flex',
+    }),
+    tag: css({
+        margin: '1rem 1rem 0 0',
+        listStyle: 'none',
+        border: '1px solid #00efe6',
+        borderRadius: '1.5em',
+        padding: '0 1rem',
+        ':hover': {
+            background: '#00efe6',
+        },
+    }),
+    link: css({
+        border: 'none',
+        ':hover': {
+            border: 'none',
+        },
+    }),
+};
+
+interface TagListProps {
+    tags: string[];
+    icon: boolean;
+}
+
+const TagList = (props: TagListProps) => {
+    const { tags } = props;
+
+    return (
+        <ul css={styles.tagsList}>
+            {tags.map((tag: string) => {
+                return (
+                    <li css={styles.tag} key={`tag-list-${tag}`}>
+                        <Link css={styles.link} to={`/tags/${tag}`}>
+                            {tag}
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default TagList;
