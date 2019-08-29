f5209ab5fe79765c9ef2d159411d0bea69f3d09c
Ankur Sheel <ankur.sheel@gmail.com>
2019-08-29 20:09:05 +1200

    Add Disqus comments

--- src/02-components/Disqus.tsx
+++ src/02-components/Disqus.tsx
@@ -1,15 +1,26 @@
+import { DiscussionEmbed } from 'disqus-react';
 import React from 'react';
-const DisqusWrapper = () => {
-    return <></>;
-    // const { disqusShortname, disqusSiteUrl } = useSiteMetadata();
-    // if (!disqusShortname || !props.slug) {
-    //     return null;
-    // }
-    // const disqusConfig = {
-    //     url: `${disqusSiteUrl}${props.slug}`,
-    //     title: props.title,
-    // };
-    // return <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />;
+import useSiteMetadata from '../hooks/use-site-config';
+
+interface DisqusProps {
+    slug?: string;
+    title?: string;
+}
+
+const DisqusWrapper = (props: DisqusProps) => {
+    const { siteUrl } = useSiteMetadata();
+    const disqusShortname = process.env.GATSBY_DISQUS_NAME;
+
+    if (!disqusShortname || !props.slug) {
+        return null;
+    }
+    const disqusConfig = {
+        identifier: props.slug,
+        title: props.title || '',
+        url: `${siteUrl}/blog/${props.slug}`,
+    };
+
+    return <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />;
 };
 
 export default DisqusWrapper;
