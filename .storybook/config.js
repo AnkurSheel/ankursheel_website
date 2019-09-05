e03af72bc41cfaa45b53e9ec431ed7983ad5ad20
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-06 09:00:16 +1200

    add addon knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 // import { withInfo } from '@storybook/addon-info';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator, configure } from '@storybook/react';
 const req = require.context('../src', true, /.stories.tsx$/);
 
@@ -21,5 +22,7 @@ addDecorator(withInfo({ inline: true, source: false }));
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(withKnobs);
 
 configure(loadStories, module);
