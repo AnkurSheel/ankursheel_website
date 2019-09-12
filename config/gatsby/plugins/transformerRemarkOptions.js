e97e58587eab93d9aff8f8d691d364d2cffffead
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-13 06:48:59 +1200

    use copy linked files plugin

--- config/gatsby/plugins/transformerRemarkOptions.js
+++ config/gatsby/plugins/transformerRemarkOptions.js
@@ -1,3 +1,12 @@
 module.exports = {
-    plugins: [`gatsby-remark-images`, `gatsby-remark-copy-linked-files`],
+    plugins: [
+        `gatsby-remark-images`,
+        {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+                destinationDir: f => `downloads/${f.name}`,
+                ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+        },
+    ],
 };
