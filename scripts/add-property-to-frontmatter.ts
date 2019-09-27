9df00fe9afd225b3bc15db580a962b18838e4f16
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-28 09:49:30 +1200

    use inquirer to make the script more flexible

--- scripts/add-property-to-frontmatter.ts
+++ scripts/add-property-to-frontmatter.ts
@@ -2,16 +2,16 @@ import prettier from 'prettier';
 import glob from 'glob';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
+import inquirer from 'inquirer';
 
-const getArticleFiles = () => {
+const getPostFiles = () => {
     return glob.sync(join(__dirname, '..', 'content', 'posts', '**', '*.mdx'));
 };
 
 const getPagesFiles = () => {
     return glob.sync(join(__dirname, '..', 'content', 'pages', '**', '*.mdx'));
 };
-
-const parseFile = async (fileName: string) => {
+const parseFile = (fileName: string, propertyName: string, propertyValue: string) => {
     let markdown = '';
 
     const content = readFileSync(fileName).toString();
@@ -19,8 +19,8 @@ const parseFile = async (fileName: string) => {
     const frontmatter = temp[1];
     const mdContent = temp[2];
 
-    const updatedFrontmatter = `\r\npublished: true${frontmatter}`;
-    markdown = prettier.format(`---\r\n${updatedFrontmatter}\r\n---\r\n${mdContent}`, {
+    const updatedFrontmatter = `\r\n${propertyName}: ${propertyValue}${frontmatter}`;
+    markdown = prettier.format(`---\r\n${updatedFrontmatter}---\r\n${mdContent}`, {
         ...require('../.prettierrc'), // eslint-disable-line global-require
         parser: 'mdx',
     });
@@ -28,9 +28,45 @@ const parseFile = async (fileName: string) => {
     console.log(`${fileName} has been updated`); // eslint-disable-line no-console
 };
 
-const main = () => {
-    getArticleFiles().map(parseFile);
-    getPagesFiles().map(parseFile);
+const main = async () => {
+    const prompt = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'propertyName',
+            message: 'Enter Property Name',
+        },
+        {
+            type: 'input',
+            name: 'properyValue',
+            message: 'Enter Property Value',
+        },
+        {
+            type: 'list',
+            name: 'posts',
+            message: 'Add to posts (yes/no)',
+            choices: [{ name: 'Yes', value: 'Y' }, { name: 'No', value: 'N' }],
+        },
+        {
+            type: 'list',
+            name: 'pages',
+            message: 'Add to pages (yes/no)',
+            choices: [{ name: 'Yes', value: 'Y' }, { name: 'No', value: 'N' }],
+        },
+    ]);
+
+    const { propertyName, properyValue, posts, pages } = prompt;
+
+    if (posts === 'Y') {
+        getPostFiles().forEach(f => {
+            parseFile(f, propertyName, properyValue);
+        });
+    }
+
+    if (pages === 'Y') {
+        getPagesFiles().forEach(f => {
+            parseFile(f, propertyName, properyValue);
+        });
+    }
 };
 
 main();
