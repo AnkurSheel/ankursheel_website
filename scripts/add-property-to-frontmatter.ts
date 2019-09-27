040883c4e1ab7b547da2ba6a648c08059ac4f8e9
Ankur Sheel <ankur.sheel@gmail.com>
2019-09-27 15:48:43 +1200

    add new scrip to add frontmatter property

---
+++ scripts/add-property-to-frontmatter.ts
@@ -0,0 +1,31 @@
+import prettier from 'prettier';
+import glob from 'glob';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+const getArticleFiles = () => {
+    return glob.sync(join(__dirname, '..', 'content', 'posts', '**', '*.mdx'));
+};
+
+const parseFile = async (fileName: string) => {
+    let markdown = '';
+
+    const content = readFileSync(fileName).toString();
+    const temp = content.split('---');
+    const frontmatter = temp[1];
+    const mdContent = temp[2];
+
+    const updatedFrontmatter = `\r\npublished: true${frontmatter}`;
+    markdown = prettier.format(`---\r\n${updatedFrontmatter}\r\n---\r\n${mdContent}`, {
+        ...require('../.prettierrc'), // eslint-disable-line global-require
+        parser: 'mdx',
+    });
+    writeFileSync(fileName, markdown);
+    console.log(`${fileName} has been updated`); // eslint-disable-line no-console
+};
+
+const main = () => {
+    getArticleFiles().map(parseFile);
+};
+
+main();
