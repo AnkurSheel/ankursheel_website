f5967edefd660d2224d76e9a2db725d7faafa9f5
Ankur Sheel <ankur.sheel@gmail.com>
2019-11-07 17:10:03 +1300

    add markdownlint to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    '*.+(md|mdx)': ['markdownlint *.mdx'],
     '*.+(js|jsx|ts|tsx)': ['eslint --fix'],
     '*.+(js|json|yml|yaml|css|less|scss|ts|tsx|md|mdx|graphql)': ['prettier --write', 'git add'],
 };
