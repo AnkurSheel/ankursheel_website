1966fdd705c4b47f3e17c23d29e2ce066a9dc4eb
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-27 09:33:17 +1300

    add initial template

---
+++ src/ankursheel_website/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using StatiqHelpers.Extensions;
+using StatiqHelpers.ImageHelpers;
+using StatiqHelpers.Pipelines;
+
+namespace ankursheel_website
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args)
+            => await Bootstrapper.Factory.CreateWeb(args)
+                .RemovePipelines()
+                .AddSetting(WebKeys.OutputPath, "../../output")
+                .AddSetting(WebKeys.CachePath, "../../cache")
+                .AddCommand<ResizeImage>()
+                .AddPipelines()
+                .AddServices()
+                .ConfigureServices(
+                    services =>
+                    {
+                        services.AddSingleton(new PostListOptions(document => document.GetPublishedDate(), Descending: true));
+                    })
+                .RunAsync();
+    }
+}
