8ac37a45cc7a753d4b09baedb3d76715aa88bcda
Ankur Sheel <ankur.sheel@gmail.com>
2024-07-10 16:30:22 +1200

    update StatiqHelpers

--- src/ankursheel_website/Program.cs
+++ src/ankursheel_website/Program.cs
@@ -2,26 +2,28 @@
 using StatiqHelpers.CustomExtensions;
 using StatiqHelpers.Pipelines;
 
-namespace ankursheel_website
-{ 
-    internal class Program
+namespace ankursheel_website;
+
+internal class Program
+{
+    public static async Task<int> Main(string[] args)
     {
-        public static async Task<int> Main(string[] args)
-            => await Bootstrapper.Factory.CreateBootstrapper(args)
-                .AddSetting(WebKeys.OutputPath, "../../output")
-                .AddSetting(WebKeys.CachePath, "../../cache")
-                .AddSetting(
-                    WebKeys.InputPaths,
-                    new[]
-                    {
-                        "input",
-                        "content"
-                    })
-                .ConfigureServices(
-                    services =>
-                    {
-                        services.AddSingleton(new PostListOptions(document => document.GetLastUpdatedDate(), true));
-                    })
-                .RunAsync();
+        return await Bootstrapper.Factory.CreateBootstrapper(args)
+            .AddSetting(WebKeys.OutputPath, "../../output")
+            .AddSetting(WebKeys.CachePath, "../../cache")
+            .AddSetting(WebKeys.InputPaths, new[]
+            {
+                "input",
+                "content"
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton(new PipelineOptions(documentList =>
+                    documentList.OrderByDescending(document => document.GetLastUpdatedDate())
+                        .ThenByDescending(document => document.GetPublishedDate())
+                        .ThenBy(document => document.GetTitle())
+                        .ToDocumentList()));
+            })
+            .RunAsync();
     }
 }
